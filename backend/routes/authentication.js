710a7cc3c50bcd51d450454157f53a740406ccb2
Alexandru Kullman <alexandrukullman@gmail.com>
2024-03-08 20:27:55 +0200

    Moved backend to another repo

--- backend/routes/authentication.js
+++
@@ -1,9 +0,0 @@
-import { login, register, logout } from "../controllers/user.js"
-import express from "express"
-const router = express.Router();
-
-router.post("/login", login);
-router.post("/register", register);
-router.post("/logout", logout);
-
-export default router;
